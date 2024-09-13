7a1e55b90e3683e85ae5fcf8c41c8150d9fac653
omar karake <omar.karake@amalitechtraining.org>
2024-09-13 16:55:37 +0200

    ft: making navigation component implementation

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.css'
+  styleUrl: './navigation.component.css',
 })
 export class NavigationComponent {
+  activeLink: string = 'home'; // Default active link
 
+  setActiveLink(link: string) {
+    this.activeLink = link;
+  }
 }

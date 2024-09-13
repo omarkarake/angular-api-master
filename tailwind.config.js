7a30c87f99f5ecb63cc950dd1dfc5e7048ecc373
omar karake <omar.karake@amalitechtraining.org>
2024-09-13 16:27:34 +0200

    chore: updating tailwind configuration and adding the icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blue-color": "#00BDD6", // For buttons and input hovers
+        "text-black": "#000000", // For general text
+        "text-secondary": "#9095A0", // For some text in cards
+        "footer-text": "#323842", // For footer text
+        "bg-primary": "#FFFFFF", // For primary background
+        "bg-secondary": "#FAFAFB", // For secondary background
+      },
+      fontFamily: {
+        epilogue: ["Epilogue", "sans-serif"],
+      },
+      fontSize: {
+        "heading-large": ["68px", "1.2"], // Heading size 68px, bold
+        "text-medium": ["20px", "1.5"], // Medium text 20px, bold
+        "text-small": ["14px", "1.4"], // Card and footer text 14px, regular
+      },
+      fontWeight: {
+        bold: "700",
+        regular: "400",
+      },
+    },
   },
   plugins: [],
 };

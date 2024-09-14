c41a0925d943ee5d9ddad72c3e954a3a4f16de57
omar karake <omar.karake@amalitechtraining.org>
2024-09-14 09:04:15 +0200

    ft: working on footer implementation

--- src/app/components/inputs/inputs.component.ts
+++ src/app/components/inputs/inputs.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-inputs',
   templateUrl: './inputs.component.html',
-  styleUrl: './inputs.component.css'
+  styleUrl: './inputs.component.css',
 })
 export class InputsComponent {
-
+  @Input() placeholder: string = ''; // Placeholder text input
+  @Input() control: FormControl = new FormControl(); // FormControl input
 }
